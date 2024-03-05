47f9ce1ebe0dabffcbb667ab9747f6266263d124
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-03-05 19:25:47 +0000

    added user_policy and generated pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  include Pundit
 
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_url)
+  end
 
   protected
 
